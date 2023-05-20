e9fdd95bfb738ca3f65949d2beca899fd7729134
wmaithil <wmaithil@gmail.com>
2023-05-20 20:57:41 +0530

    Update Body.js

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,14 +1,25 @@
 import { React,useState,useEffect } from "react";
 import RestaurantCard from "./RestaurantCard"; 
+import Shimmer from "./Shimmer";
+
+const filterRestaurants = (key, listValues)=>{
+  const filterList = listValues.filter(i=>{
+    return(
+      i?.data?.name?.toUpperCase()?.includes(key.toUpperCase())
+    )
+  });
+  return filterList;
+};
 
 const Body = ()=>{
   
     const searchInputStyle={
       padding:"10px 15px",width:"20rem",margin:"0 2rem",border:"1px solid #0f0f0f",borderRadius:"1.5rem"
     }
+    const [allRestaurantList,setallRestaurantList] = useState([]);
     const [restaurantList,setrestaurantList] = useState([]);
     const [inputVal,setinputVal] = useState("");
-    
+
     useEffect(()=>{
       getData();
     },[])
@@ -16,36 +27,42 @@ const Body = ()=>{
     //function to call Api
     async function getData(){
       //fetch data from API 
-      const resApiData= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat={latitude}&lng={longitude}&page_type=DESKTOP_WEB_LISTING");
+      const resApiData= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.225535&lng=72.845748&page_type=DESKTOP_WEB_LISTING");
       const resData= await resApiData.json();
-      console.log(resData);
       //storing the restaurant list
       const resList= resData?.data?.cards[2]?.data?.data?.cards;
       console.log("Res List",resList);
       //update the state of restaurantList 
+      setallRestaurantList(resList);
       setrestaurantList(resList);
     }
-  
-    return (
-      <div className="body">
+
+    return (restaurantList.length===0) ? (
+      <Shimmer/>
+    ) : (
+      <>
         <div className="search">
           <input style={searchInputStyle} 
             type="text" 
             value={inputVal} 
             onChange={(e)=> setinputVal(e.target.value)}
           />
-          <button type="submit" className="filterbtn">Filter </button>
+          <button type="submit" onClick={()=>{
+              const filterList= filterRestaurants(inputVal,allRestaurantList)
+              setrestaurantList(filterList);
+            }
+          } className="filterbtn">Filter </button>
         </div>
         <div className="resContainer">
           {
             restaurantList.map((i) => {
               return (
-                <RestaurantCard key={i.data.id} props={i.data}/>
+                <RestaurantCard key={i.data.id} {...i.data}/>
               )
             })
           }
         </div>
-      </div>
+      </>
     )
 }
 
