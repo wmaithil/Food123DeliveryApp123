5ba7338dd9d4d42a9a0aed488be8b8a6ee786724
wmaithil <wmaithil@gmail.com>
2023-05-20 21:02:20 +0530

    Create Shimmer.js

---
+++ src/Components/Shimmer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <>
+        <div className="resCard">
+        <img style={{width:"265px",height:"175px",borderRadius:"9px",backgroundColor:"#f6f6f6"}}  alt=""/>
+          
+      </div>
+    </>
+  )
+};
+
+export default Shimmer;
