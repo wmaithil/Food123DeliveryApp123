578518683b50e558f0eceae5346d005ae6f34308
wmaithil <wmaithil@gmail.com>
2023-06-03 13:24:10 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,1 +1,13 @@
+import React from 'react'
 
+const About = () => {
+  return (
+    <div>
+         <h1>About Us</h1>
+         
+         <h2>We are amazing food delivery Application</h2>
+    </div>
+  )
+}
+
+export default About;
