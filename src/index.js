49babaf652c54d978825abec9b62ae6208076068
wmaithil <wmaithil@gmail.com>
2023-06-03 13:25:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import About from "./Components/About";
+import Error from "./Components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const appRouter = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<App/>,
+      errorElement:<Error/>
+    },
+    {
+      path:"/about",
+      element:<About/>
+    }
+  ]
 );
+root.render( <RouterProvider router={appRouter}/> );
 
 
