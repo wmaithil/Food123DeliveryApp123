0364a4be96a86a0cb6801276f8c26ba1745b3867
wmaithil <wmaithil@gmail.com>
2023-06-03 13:24:41 +0530

    Create Error.js

---
+++ src/Components/Error.js
@@ -0,0 +1,14 @@
+import {useRouteError} from "react-router-dom";
+
+const Error=()=>{
+    const err = useRouteError();
+    console.log(err);
+    return (
+        <div>
+            <h2>{err.status + "! "+ err.statusText}</h2>
+            <h4>{ err.error.message}</h4>
+        </div>
+    )
+}
+
+export default Error; 
