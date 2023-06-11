dd6a174bda7ad4fba513e9656ca392cce2e43e4c
wmaithil <wmaithil@gmail.com>
2023-06-11 21:05:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './App.css';
+import Header from './Components/Header';
 import About from "./Components/About";
 import Error from "./Components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./Components/Contact";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Body from './Components/Body';
+import RestaurantMenu from './Components/RestaurantMenu';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const App= ()=> {
+  return (
+    <div className="App">
+        <Header/>
+        <Outlet/>
+    </div>
+  );
+}
 
 const appRouter = createBrowserRouter(
   [
     {
       path:"/",
       element:<App/>,
+      children:[
+        {
+          path:"/",
+          element:<Body/>
+        },
+        {
+          path:"/about",
+          element:<About/>
+        },
+        {
+          path: "/contact",
+          element:<Contact/>
+        },
+        {
+          path:"/restaurants/:resId",
+          element:<RestaurantMenu/>
+        }
+      ],
       errorElement:<Error/>
     },
-    {
-      path:"/about",
-      element:<About/>
-    }
+    
   ]
 );
 root.render( <RouterProvider router={appRouter}/> );
-
 
