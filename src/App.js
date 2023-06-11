cc988a3d61026ae37bfe7c9f97b1fbe50f95bcdd
wmaithil <wmaithil@gmail.com>
2023-06-11 21:06:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,1 @@
-import { React } from 'react';
-import './App.css';
-import Header from './Components/Header';
-import Body from './Components/Body';
 
-const App= ()=> {
-  return (
-    <div className="App">
-        <Header/>
-        <Body/>
-    </div>
-  );
-}
-
-export default App;
