99ea3cb367dd0f173cffe1332c8fd1af28756e79
wmaithil <wmaithil@gmail.com>
2023-07-05 22:47:55 +0530

    Update Error.js

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,12 +1,13 @@
 import {useRouteError} from "react-router-dom";
 
+
 const Error=()=>{
     const err = useRouteError();
     console.log(err);
     return (
         <div>
-            <h2>{err.status + "! "+ err.statusText}</h2>
-            <h4>{ err.error.message}</h4>
+            <h2>{err?.status + "! "+ err?.statusText}</h2>
+            <h4>{err?.error?.message}</h4>
         </div>
     )
 }
