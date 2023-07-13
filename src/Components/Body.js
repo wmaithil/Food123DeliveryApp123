b23697f80a9968c9b502e44863b01892d26bc645
wmaithil <wmaithil@gmail.com>
2023-07-13 13:36:32 +0530

    Update Body.js

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,6 +4,7 @@ import Shimmer from "./Shimmer";
 import {Link} from "react-router-dom";
 import {RestaurantList_API} from "../Utils/Constants";
 import useOnlineStatus from "../Utils/useOnlineStatus";
+
 const filterRestaurants = (key, listValues)=>{
   console.log("list",listValues);
   const filterList = listValues.filter(i=>{
@@ -16,10 +17,7 @@ const filterRestaurants = (key, listValues)=>{
 };
 
 const Body = ()=>{
-  
-    const searchInputStyle={
-      padding:"10px 15px",width:"20rem",margin:"0 2rem",borderRadius:"1.5rem", boxShadow: "0 2px 3px 2px #f6f6f6"
-    }
+
     const [allRestaurantList,setallRestaurantList] = useState([]);
     const [restaurantList,setrestaurantList] = useState([]);
     const [inputVal,setinputVal] = useState("");
@@ -33,8 +31,10 @@ const Body = ()=>{
       //fetch data from API 
       const resApiData= await fetch(RestaurantList_API);
       const resData= await resApiData.json();
+
       //storing the restaurant list
       const resList= resData?.data?.cards[2]?.data?.data?.cards;
+      
       console.log("Res List",resList);
       //update the state of restaurantList 
       setallRestaurantList(resList);
@@ -46,17 +46,15 @@ const Body = ()=>{
     return (
       <>
         <div className="search">
-          <input style={searchInputStyle} 
-            type="text" 
-            value={inputVal} 
-            onChange={(e)=> setinputVal(e.target.value)}
-          />
+          <input type="text" value={inputVal}  onChange={(e)=> setinputVal(e.target.value)} 
+          className="border border-solid border-black mx-4 py-1 px-2" />
+
           <button type="submit" 
             onClick={()=>{
               const filterList= filterRestaurants(inputVal,allRestaurantList)
               setrestaurantList(filterList);
             }}
-            className="filterbtn"
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
             >Filter </button>
           
           {isUserOnline ? <h1> Online</h1> : <h1>Offline</h1>}
