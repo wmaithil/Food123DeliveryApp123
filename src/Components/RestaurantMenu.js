48cc01d1703eff0f8ab754790302cdab67362e73
wmaithil <wmaithil@gmail.com>
2023-07-24 00:58:51 +0530

    Update RestaurantMenu.js

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,36 +1,42 @@
 import Shimmer from "./Shimmer";
 import {useParams} from "react-router-dom";
 import useRestaurantMenu from "../Utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
     const {resId} = useParams();
     const menuData = useRestaurantMenu(resId);
+    const [showIndex, setShowIndex] = useState(0);
 
-if(menuData=== null) return <Shimmer/>;
+    if(menuData=== null) return <Shimmer/>;
 
+    const {name,cuisines,costForTwoMessage} = menuData?.cards[0]?.card?.card?.info;
 
-const {name,cuisines,costForTwoMessage} = menuData?.cards[0]?.card?.card?.info;
+    // const itemCards = menuData?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
 
-const itemCards = menuData?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
-console.log("Cards",itemCards);
+    const Categories = menuData?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(i=> i.card?.card?.["@type"]=== "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+    console.log("Cate",Categories);
 
-  return (
-    <div className="menu">
-   
-        <h1>{name}</h1>
-        <h3>{cuisines.join(", ")}</h3>
-        <h3>{costForTwoMessage}</h3>
-        <ul>
-            {itemCards.map(item =>{
-                return(
-                    <li key={item.card.info.id} >
-                        {item.card.info.name} - Rs. {item.card.info.price/100 || item.card.info.defaultPrice/100}
-                    </li>
-                )
-            })}
-        </ul>
-    </div>
-  )
+    return (
+        <div className="text-center">
+            <h1 className="font-bold my-4 text-2xl">{name}</h1>
+            <h3 className="font-bold text-lg">{cuisines.join(", ")}</h3>
+            <h3>{costForTwoMessage}</h3>
+            
+            {
+                Categories.map((i,index) =>{
+                    return (
+                        <RestaurantCategory key={i.card.card.title} 
+                            data={i.card.card} 
+                            showIndex={index === showIndex}
+                            setShowIndex={() => setShowIndex(index)}
+                            />
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default RestaurantMenu;
