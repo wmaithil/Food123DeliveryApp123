202cf9749629f9bb0cbd02c249b4170f456e532a
wmaithil <wmaithil@gmail.com>
2023-09-24 13:38:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './App.css';
 import Header from './Components/Header';
 import About from "./Components/About";
 import Error from "./Components/Error";
 import Contact from "./Components/Contact";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Body from './Components/Body';
-import RestaurantMenu from './Components/RestaurantMenu';
+import RestaurantMenu from "./Components/RestaurantMenu";
+import { Provider } from "react-redux";
+import Store from './Utils/Store';
+import Cart from './Components/Cart';
+import UserContext from './Utils/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const App= ()=> {
+  const [searchValue, setSearchValue] = useState("");
+  const [btnClicked, setBtnClicked] = useState(false);
   return (
-    <div className="App">
-        <Header/>
-        <Outlet/>
-    </div>
+    <Provider store={Store}>
+      <UserContext.Provider value={{searchText : searchValue, buttonClicked : btnClicked, setBtnClicked, setSearchValue}}>
+        <div className="App">
+            <Header/>
+            <Outlet/>
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 }
 
@@ -44,6 +53,10 @@ const appRouter = createBrowserRouter(
         {
           path: "/grocery",
           element: <Suspense fallback={<h1>Loading Grocery page</h1>}> <Grocery/> </Suspense>
+        },
+        {
+          path:"/cart",
+          element: <Cart/>
         },
         {
           path:"/restaurants/:resId",
