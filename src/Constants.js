1fac884b5287d9d172835435d8f7294609ab901c
wmaithil <wmaithil@gmail.com>
2023-09-24 13:38:45 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,1 @@
 
-export const RestaurantList_API= "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.225535&lng=72.845748&page_type=DESKTOP_WEB_LISTING";
-
-export const Menu_API= "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.2307329&lng=72.856673&restaurantId=";
